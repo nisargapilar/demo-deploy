f494483d1e523f3ffb4b7a6fdaef15509c0d2c1f
nisargapilar <pilar.nisarga@gmail.com>
2025-11-18 22:44:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function showLove() {
+    const messages = [
+        "You're amazing! 🌟",
+        "Do your best! 🐰",
+        "Be possitive! 🌸",
+        "Always smile! 😊",
+        "Help those in need! 🐾"
+    ];
+    
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    document.getElementById('message').textContent = randomMessage;
+    
+    // Add some cute effects
+    document.body.style.background = getRandomPastelColor();
+}
+
+function getRandomPastelColor() {
+    const colors = [
+        '#ffe6f2', '#e6f2ff', '#e6ffe6', 
+        '#fff0e6', '#f0e6ff', '#e6ffff'
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
